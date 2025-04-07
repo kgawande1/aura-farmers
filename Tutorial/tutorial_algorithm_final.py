1c7a2e79c9c4a1de30b9f64eb7171411e3b5a3f6
kgawande1 <krishgawande25@gmail.com>
2025-04-06 21:00:37 -0400

    created our final algorithm for the tutorial

---
+++ Tutorial/tutorial_algorithm_final.py
@@ -0,0 +1,26 @@
+# Our final tutorial algorithm
+
+
+from datamodel import Order, OrderDepth, TradingState
+from datamodel import Listing, Observation, Order, OrderDepth, ProsperityEncoder, Symbol, Trade, TradingState
+from typing import List, Dict, Any
+import json
+
+
+
+class Trader:
+
+    def run(self, state: TradingState) -> Dict[str, List[Order]]:
+        result: Dict[str, List[Order]] = {}
+        conversions = 0
+        traderData = ""
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        return result, conversions, traderData
