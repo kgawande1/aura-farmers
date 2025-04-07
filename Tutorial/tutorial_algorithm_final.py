2bde8d2a4d7dcb59d1aef0820ad94d7063181ff8
theflashwin <theflashwin1@gmail.com>
2025-04-06 22:40:15 -0400

    added working code

--- Tutorial/tutorial_algorithm_final.py
+++ Tutorial/tutorial_algorithm_final.py
@@ -14,19 +14,20 @@ class Trader:
 
         default_value: float = 0.0
         diff: int = 1
-        max_ticks: int = 10
+        max_ticks: int = 5
         
-        if product not in price_cache or len(price_cache[product]) < 10:
+        if product not in price_cache or len(price_cache[product]) <= 2 * max_ticks:
             if product in price_cache:
                 return price_cache[product][-1]
             else:
-                return default_value # fix later
+                return default_value
             
         prices = np.array(price_cache[product])
         diffed_prices = np.diff(prices, n=diff)
 
-        n = len(prices)
-        X = [], y = []
+        n = len(diffed_prices)
+        X = []
+        y = []
 
         for t in range(max_ticks, n):
             X.append(diffed_prices[t-max_ticks:t][::-1])
@@ -38,7 +39,7 @@ class Trader:
         phi, residuals, rank, s = np.linalg.lstsq(X, y, rcond=None)
 
         # Forecast the next difference using the last p differences.
-        last_p = diffed_prices[-p:][::-1]
+        last_p = diffed_prices[-max_ticks:][::-1]
         forecast_diff = np.dot(phi, last_p)
         # The forecasted price is the last observed price plus the forecasted difference.
         forecast_price = prices[-1] + forecast_diff
@@ -48,15 +49,22 @@ class Trader:
     def run(self, state: TradingState) -> Dict[str, List[Order]]:
         result: Dict[str, List[Order]] = {}
         conversions = 0
-        traderData = jsonpickle.decode(state.traderData)
 
-        price_cache = traderData["price_cache"]
+        try:
+            traderData = jsonpickle.decode(state.traderData)
+        except Exception as _:
+            traderData = {}
+
+        price_cache: Dict[str: List[float]] = traderData["price_cache"] if "price_cache" in traderData else {}
 
         for product in state.order_depths:
 
             orders: List[Order] = []
             order_depth: OrderDepth = state.order_depths[product]
             fair_price = self.get_fair_value(product, price_cache)
+
+            if fair_price == 0:
+                continue
 
             new_price = 0
 
@@ -78,7 +86,14 @@ class Trader:
                     if sell_qty > 0:
                         orders.append(Order(product, bid_price, -sell_qty))
 
-            price_cache[product].add(new_price / len(order_depth.sell_orders))
+            if product not in price_cache:
+                price_cache[product] = []
+
+            best_bid = max(order_depth.buy_orders.keys())
+            best_ask = min(order_depth.sell_orders.keys())
+            mid_price = (best_bid + best_ask) / 2.0
+
+            price_cache[product].append(mid_price)
 
             result[product] = orders
 
