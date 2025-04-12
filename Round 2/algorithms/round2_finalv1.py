daf2a5814c186b84982ba9dc8d494e4b5a4174a2
kgawande1 <krishgawande25@gmail.com>
2025-04-12 03:20:29 -0400

    added new

---
+++ Round 2/algorithms/round2_finalv1.py
@@ -0,0 +1,470 @@
+# Our algorithm for round1 final
+
+import json
+from abc import abstractmethod
+from collections import deque
+from datamodel import Listing, Observation, Order, OrderDepth, ProsperityEncoder, Symbol, Trade, TradingState
+from typing import Any, List, TypeAlias
+import numpy as np
+import jsonpickle
+
+JSON: TypeAlias = dict[str, "JSON"] | list["JSON"] | str | int | float | bool | None
+
+class Logger:
+    def __init__(self) -> None:
+        self.logs = ""
+        self.max_log_length = 3750
+
+    def print(self, *objects: Any, sep: str = " ", end: str = "\n") -> None:
+        self.logs += sep.join(map(str, objects)) + end
+
+    def flush(self, state: TradingState, orders: dict[Symbol, list[Order]], conversions: int, trader_data: str) -> None:
+        base_length = len(self.to_json([
+            self.compress_state(state, ""),
+            self.compress_orders(orders),
+            conversions,
+            "",
+            "",
+        ]))
+
+        # We truncate state.traderData, trader_data, and self.logs to the same max. length to fit the log limit
+        max_item_length = (self.max_log_length - base_length) // 3
+
+        print(self.to_json([
+            self.compress_state(state, self.truncate(state.traderData, max_item_length)),
+            self.compress_orders(orders),
+            conversions,
+            self.truncate(trader_data, max_item_length),
+            self.truncate(self.logs, max_item_length),
+        ]))
+
+        self.logs = ""
+
+    def compress_state(self, state: TradingState, trader_data: str) -> list[Any]:
+        return [
+            state.timestamp,
+            trader_data,
+            self.compress_listings(state.listings),
+            self.compress_order_depths(state.order_depths),
+            self.compress_trades(state.own_trades),
+            self.compress_trades(state.market_trades),
+            state.position,
+            self.compress_observations(state.observations),
+        ]
+
+    def compress_listings(self, listings: dict[Symbol, Listing]) -> list[list[Any]]:
+        compressed = []
+        for listing in listings.values():
+            compressed.append([listing.symbol, listing.product, listing.denomination])
+
+        return compressed
+
+    def compress_order_depths(self, order_depths: dict[Symbol, OrderDepth]) -> dict[Symbol, list[Any]]:
+        compressed = {}
+        for symbol, order_depth in order_depths.items():
+            compressed[symbol] = [order_depth.buy_orders, order_depth.sell_orders]
+
+        return compressed
+
+    def compress_trades(self, trades: dict[Symbol, list[Trade]]) -> list[list[Any]]:
+        compressed = []
+        for arr in trades.values():
+            for trade in arr:
+                compressed.append([
+                    trade.symbol,
+                    trade.price,
+                    trade.quantity,
+                    trade.buyer,
+                    trade.seller,
+                    trade.timestamp,
+                ])
+
+        return compressed
+
+    def compress_observations(self, observations: Observation) -> list[Any]:
+        conversion_observations = {}
+        for product, observation in observations.conversionObservations.items():
+            conversion_observations[product] = [
+                observation.bidPrice,
+                observation.askPrice,
+                observation.transportFees,
+                observation.exportTariff,
+                observation.importTariff,
+                observation.sugarPrice,
+                observation.sunlightIndex,
+            ]
+
+        return [observations.plainValueObservations, conversion_observations]
+
+    def compress_orders(self, orders: dict[Symbol, list[Order]]) -> list[list[Any]]:
+        compressed = []
+        for arr in orders.values():
+            for order in arr:
+                compressed.append([order.symbol, order.price, order.quantity])
+
+        return compressed
+
+    def to_json(self, value: Any) -> str:
+        return json.dumps(value, cls=ProsperityEncoder, separators=(",", ":"))
+
+    def truncate(self, value: str, max_length: int) -> str:
+        if len(value) <= max_length:
+            return value
+
+        return value[:max_length - 3] + "..."
+
+logger = Logger()
+
+class Strategy:
+    def __init__(self, symbol: str, limit: int) -> None:
+        self.symbol = symbol
+        self.limit = limit
+
+    @abstractmethod
+    def act(self, state: TradingState) -> None:
+        raise NotImplementedError()
+
+    def run(self, state: TradingState) -> list[Order]:
+        self.orders = []
+        self.act(state)
+        return self.orders
+
+    def buy(self, price: int, quantity: int) -> None:
+        self.orders.append(Order(self.symbol, price, quantity))
+
+    def sell(self, price: int, quantity: int) -> None:
+        self.orders.append(Order(self.symbol, price, -quantity))
+
+    def save(self) -> JSON:
+        return None
+
+    def load(self, data: JSON) -> None:
+        pass
+
+class MarketMakingStrategy(Strategy):
+    def __init__(self, symbol: Symbol, limit: int) -> None:
+        super().__init__(symbol, limit)
+
+        self.window = deque()
+        self.window_size = 10
+
+    @abstractmethod
+    def get_true_value(state: TradingState) -> int:
+        raise NotImplementedError()
+
+    def act(self, state: TradingState) -> None:
+        true_value = self.get_true_value(state)
+
+        order_depth = state.order_depths[self.symbol]
+        buy_orders = sorted(order_depth.buy_orders.items(), reverse=True)
+        sell_orders = sorted(order_depth.sell_orders.items())
+
+        position = state.position.get(self.symbol, 0)
+        to_buy = self.limit - position
+        to_sell = self.limit + position
+
+        self.window.append(abs(position) == self.limit)
+        if len(self.window) > self.window_size:
+            self.window.popleft()
+
+        soft_liquidate = len(self.window) == self.window_size and sum(self.window) >= self.window_size / 2 and self.window[-1]
+        hard_liquidate = len(self.window) == self.window_size and all(self.window)
+
+        max_buy_price = true_value - 1 if position > self.limit * 0.5 else true_value
+        min_sell_price = true_value + 1 if position < self.limit * -0.5 else true_value
+
+        for price, volume in sell_orders:
+            if to_buy > 0 and price <= max_buy_price:
+                quantity = min(to_buy, -volume)
+                self.buy(price, quantity)
+                to_buy -= quantity
+
+        if to_buy > 0 and hard_liquidate:
+            quantity = to_buy // 2
+            self.buy(true_value, quantity)
+            to_buy -= quantity
+
+        if to_buy > 0 and soft_liquidate:
+            quantity = to_buy // 2
+            self.buy(true_value - 2, quantity)
+            to_buy -= quantity
+
+        if to_buy > 0:
+            popular_buy_price = max(buy_orders, key=lambda tup: tup[1])[0]
+            price = min(max_buy_price, popular_buy_price + 1)
+            self.buy(price, to_buy)
+
+        for price, volume in buy_orders:
+            if to_sell > 0 and price >= min_sell_price:
+                quantity = min(to_sell, volume)
+                self.sell(price, quantity)
+                to_sell -= quantity
+
+        if to_sell > 0 and hard_liquidate:
+            quantity = to_sell // 2
+            self.sell(true_value, quantity)
+            to_sell -= quantity
+
+        if to_sell > 0 and soft_liquidate:
+            quantity = to_sell // 2
+            self.sell(true_value + 2, quantity)
+            to_sell -= quantity
+
+        if to_sell > 0:
+            popular_sell_price = min(sell_orders, key=lambda tup: tup[1])[0]
+            price = max(min_sell_price, popular_sell_price - 1)
+            self.sell(price, to_sell)
+
+    def save(self) -> JSON:
+        return list(self.window)
+
+    def load(self, data: JSON) -> None:
+        self.window = deque(data)
+
+class RainforestResinStrategy(MarketMakingStrategy):
+    def get_true_value(self, state: TradingState) -> int:
+        return 10000
+    
+    
+class DjembesStrategy(MarketMakingStrategy):
+
+    def __init__(self, symbol: Symbol, limit: int) -> None:
+        super().__init__(symbol, limit)
+
+        self.tiemsteps_into_future = 50
+        self.placeholder_variance = 0.05
+        self.random_seed = 42
+
+        # distribution params
+        self.brownian_mean_1 = 0
+        self.brownian_variance_1 = 1
+        self.brownian_mean_2 = 0
+        self.brownian_variance_2 = 0
+
+        # greeks
+        self.rho = 0          # correlation
+        self.kappa = 2.0        # mean reversion tendency
+        self.theta = 0.2        # long term variance
+        self.xi = 0.3           # volatility of the volatility
+        
+        self.r = 0 # risk return (no return in our case)
+
+        self.bias = -3806.6215702688423
+
+    def get_price_cache(self, state: TradingState) -> list:
+
+        try:
+            trader_data = jsonpickle.decode(state.traderData)
+        except Exception as _:
+            trader_data = {}
+
+        return trader_data["price_cache"] if "price_cache" in trader_data else {}
+
+    def get_true_value(self, state: TradingState) -> int:
+
+        prices = self.get_price_cache(state=state)
+        
+        # n is the number of future timesteps we want to predict
+        k = len(prices)
+        n = self.tiemsteps_into_future + k
+        predictions = np.full(n, np.nan)
+        predictions[:k] = prices[:k]
+
+        # get the variance of the past k timesteps
+        log_returns = np.diff(np.log(prices[:k+1]))
+        v = np.full(n, np.nan)
+        v[:k] = np.var(log_returns) if len(log_returns) > 1 else self.placeholder_variance
+
+        np.random.seed(self.random_seed)
+
+        for t in range(k, n):
+
+            past_prices = prices[t-k:t]
+            past_returns = np.diff(np.log(past_prices))
+            v_mean = np.var(past_returns) if len(past_returns) > 1 else self.placeholder_variance
+            trend = (past_prices[-1] - past_prices[0]) / k
+
+            Z1 = np.random.normal(loc=self.brownian_mean_1, scale=self.brownian_variance_1)
+            Z2 = np.random.normal(loc=self.brownian_mean_2, scale=self.brownian_variance_2)
+
+            W1 = Z1
+            W2 = self.rho * Z1 + np.sqrt(1 - self.rho**2) * Z2
+
+            dt = 1/n
+            v[t] = np.maximum(
+                v[t-1] + self.kappa * (self.theta - v[t-1]) * dt + self.xi * np.sqrt(np.max(v[t-1], 0)) * np.sqrt(dt) * W2,
+                0
+            )
+
+            predictions[t] = predictions[t-1] * np.exp(
+                (self.r + 0.3 * trend / predictions[t-1] - 0.5 * v_mean) * dt +
+                np.sqrt(v_mean) * np.sqrt(dt) * W1
+            )
+
+        return predictions[-1] + self.bias
+
+class KelpStrategy(MarketMakingStrategy):
+    def get_true_value(self, state: TradingState) -> int:
+        order_depth = state.order_depths[self.symbol]
+
+        order_depth = state.order_depths[self.symbol]
+        buy_orders = sorted(order_depth.buy_orders.items(), reverse=True)
+        sell_orders = sorted(order_depth.sell_orders.items())
+
+        popular_buy_price = max(buy_orders, key=lambda tup: tup[1])[0]
+        popular_sell_price = min(sell_orders, key=lambda tup: tup[1])[0]
+
+        return round((popular_buy_price + popular_sell_price) / 2)
+    
+
+class SquidInkStrategy(MarketMakingStrategy):
+    def get_true_value(self, state: TradingState) -> int:
+        try:
+            traderData = jsonpickle.decode(state.traderData)
+        except Exception as _:
+            traderData = {}
+
+        if traderData is None:
+            traderData = {}
+
+        price_cache = traderData["price_cache"] if "price_cache" in traderData else {}
+        k = 200
+        
+        def exponential_moving_average(prices, alpha=0.3):
+            ema = prices[0]
+            for price in prices[1:]:
+                ema = alpha * price + (1 - alpha) * ema
+            return ema
+    
+        def get_trend(prices):
+            if len(prices) < 2:
+                return 0
+            x = np.arange(len(prices))
+            y = np.array(prices)
+
+            slope, _ = np.polyfit(x, y, 1)
+            return slope
+
+        def get_fair_value_merton(
+                T: float,
+                mu: float, 
+                lamb: float, 
+                sigma: float, 
+                v: float,
+                delta: float, 
+                prev_prices: List[float],
+                mu_w: float,
+                sigma_w: float,
+                n: int = 100,
+                beta: float = 0.47):
+            
+            # n -> number of simulations
+            # m -> number of future walk
+
+            kappa = np.exp(v + 0.5 * pow(delta, 2)) - 1
+            avg = 0
+
+            # get average price
+            weights = np.random.normal(loc=mu_w, scale=sigma_w, size=len(prev_prices))
+            weights = np.ones(shape=len(prev_prices))
+            normalized_weights = weights / np.sum(weights)
+
+            prev_price = np.dot(normalized_weights, prev_prices)
+
+            ema = exponential_moving_average(prices=prev_prices)
+            trend = get_trend(prices=prev_prices)
+
+            prev_price = ema + trend * 3
+
+            for i in range(n):
+
+                W_T = np.random.normal(0, np.sqrt(T)) # brownian motion
+
+                # Number of jumps (Poisson)
+                N_T = np.random.poisson(lamb * T)
+
+                # Sum of jump magnitudes (log-normal in log-space)
+                jump_sum = np.sum(np.random.normal(v, delta, size=N_T)) if N_T > 0 else 0.0
+
+                # Combine terms
+                drift = (mu - lamb * kappa - 0.5 * sigma**2) * T
+                diffusion = sigma * W_T
+
+                log_S = np.log(prev_price) + (drift + diffusion + jump_sum) * beta
+                S_T = np.exp(log_S)
+                avg += S_T
+
+            return 2000 - (avg / n - 2000)
+        
+        ink_value = get_fair_value_merton(
+            T=5,
+            mu=0.0336,#does
+            lamb=0.0000000001,
+            sigma=0.000009,#does
+            v=1.0000000,
+            delta=5.000000,
+            prev_prices= list(price_cache["SQUID_INK"]) if "SQUID_INK" in price_cache else [2000.0],   # FIX: Pass a float instead of the entire price_cache
+            mu_w=0.5,
+            sigma_w=0.5)
+        
+        if "SQUID_INK" not in price_cache:
+            price_cache["SQUID_INK"] = deque()
+        
+        order_depth: OrderDepth = state.order_depths["SQUID_INK"]
+        best_bid = max(order_depth.buy_orders.keys())
+        best_ask = min(order_depth.sell_orders.keys())
+        mid_price = (best_bid + best_ask) / 2
+
+        if len(price_cache["SQUID_INK"]) < k:
+            price_cache["SQUID_INK"].append(mid_price)
+        else:
+            price_cache["SQUID_INK"].popleft()
+            price_cache["SQUID_INK"].append(mid_price)
+       
+        traderData = jsonpickle.encode({
+            "price_cache": price_cache
+        })
+        return round(ink_value)
+    
+
+
+class Trader:
+    def __init__(self) -> None:
+        limits = {
+            "RAINFOREST_RESIN": 50,
+            "KELP": 50,
+            "SQUID_INK" : 50,
+            "DJEMBES": 50
+        }
+
+        self.strategies = {symbol: clazz(symbol, limits[symbol]) for symbol, clazz in {
+            "RAINFOREST_RESIN": RainforestResinStrategy,
+            "KELP": KelpStrategy,
+            "SQUID_INK" : SquidInkStrategy,
+            "DJEMBES": DjembesStrategy,
+        }.items()}
+
+    def run(self, state: TradingState) -> tuple[dict[Symbol, list[Order]], int, str]:
+        logger.print(state.position)
+
+        conversions = 0
+        
+        old_trader_data = json.loads(state.traderData) if state.traderData != "" else {}
+        new_trader_data = {}
+
+        orders = {}
+        for symbol, strategy in self.strategies.items():
+            if symbol in old_trader_data:
+                strategy.load(old_trader_data.get(symbol, None))
+
+            if symbol in state.order_depths:
+                orders[symbol] = strategy.run(state)
+
+            new_trader_data[symbol] = strategy.save()
+            
+
+        trader_data = json.dumps(new_trader_data, separators=(",", ":"))
+        
+
+        logger.flush(state, orders, conversions, trader_data)
+        return orders, conversions, trader_data
